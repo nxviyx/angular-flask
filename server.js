0a0f37573cb3c3004b39b1ff8172822dedf5cdc8
Naveen Fernando <malinda0610@gmail.com>
2021-05-27 15:14:13 +0530

    first commit

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/frontend'));
+app.use(express.static(__dirname + '/dist/frontend'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/frontend/'}),
-);
+app.get('/*', function(req,res) {
 
-// Start the app by listening on the default Heroku port
+res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
