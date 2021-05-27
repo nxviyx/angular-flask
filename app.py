335720db5a462ccfbe6cffcd58dc7443ff072883
Naveen Fernando <malinda0610@gmail.com>
2021-05-27 13:25:12 +0530

    first commit

--- app.py
+++ app.py
@@ -13,30 +13,52 @@
 #
 # api.add_resource(HelloApiHandler, '/flask/hello')
 
-from flask import Flask, send_from_directory
+# from flask import Flask, send_from_directory
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/<path:path>', methods=['GET'])
+# def static_proxy(path):
+#   return send_from_directory('./', path)
+#
+#
+# @app.route('/')
+# def root():
+#   return send_from_directory('./', 'index.html')
+#
+# @app.route('/flask/hello')
+# def test():
+#   return "Hellow from heroku"
+#
+# if __name__ == '__main__':
+#   # This is used when running locally only. When deploying use a webserver process
+#   # such as Gunicorn to serve the app.
+#   app.run(host='127.0.0.1', port=8080, debug=True)
+#
+#
+# @app.errorhandler(500)
+# def server_error(e):
+#   return 'An internal error occurred [main.py] %s' % e, 500
 
-app = Flask(__name__)
 
+from flask import Flask,render_template, request, jsonify
+from flask_compress import Compress
 
-@app.route('/<path:path>', methods=['GET'])
-def static_proxy(path):
-  return send_from_directory('./', path)
+# initialize flask application
+app = Flask(__name__,static_url_path='',static_folder='dist',template_folder='dist')
+Compress(app)
 
 
 @app.route('/')
-def root():
-  return send_from_directory('./', 'index.html')
+def test():
+    return render_template("index.html")
 
 @app.route('/flask/hello')
-def test():
+def testing():
   return "Hellow from heroku"
 
 if __name__ == '__main__':
-  # This is used when running locally only. When deploying use a webserver process
-  # such as Gunicorn to serve the app.
-  app.run(host='127.0.0.1', port=8080, debug=True)
+    # run web server
+    app.run()
 
-
-@app.errorhandler(500)
-def server_error(e):
-  return 'An internal error occurred [main.py] %s' % e, 500
