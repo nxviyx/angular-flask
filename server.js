6f4576d9641d2071dc0084b79e95208e9c95902a
Naveen Fernando <malinda0610@gmail.com>
2021-05-27 14:57:16 +0530

    first commit

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-flask'));
+app.use(express.static('./dist/frontend'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname,'/dist/angular-flask/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/frontend/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
